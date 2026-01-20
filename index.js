97c402fde90958e4a443f117037dd752c731ab4a
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-01-20 17:07:39 +0700

    i forgot to remove the hints from NPM Doc

--- index.js
+++ index.js
@@ -4,22 +4,18 @@ import fs from "fs";
 
 
 inquirer.prompt([
-    /* Pass your questions in here */
     {
         message: "Type url here: ",
         name: "url",
     },
   ])
   .then((answers) => {
-        // Use user feedback for... whatever!!
         const typeUrl = answers.url;
         var qr_png = qr.image(typeUrl);
                      qr_png.pipe(fs.createWriteStream('arifInstagram.png'));
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+      } else {
+      }
   });
