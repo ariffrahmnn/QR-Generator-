7bf08d7780b5fd92702513a229e7b6ebe5730966
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-01-21 13:16:37 +0700

    Added an information when the file has been created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import inquirer from "inquirer";
+
 import qr from "qr-image";
 import fs from "fs";
 
@@ -12,7 +13,12 @@ inquirer.prompt([
   .then((answers) => {
         const typeUrl = answers.url;
         var qr_png = qr.image(typeUrl);
-                     qr_png.pipe(fs.createWriteStream('arifInstagram.png'));
+        qr_png.pipe(fs.createWriteStream('googel.png'));
+        fs.readFile('readme.txt', 'utf8', (err, answers) => {
+            if (err) throw err;
+            console.log(answers);
+        });
+
   })
   .catch((error) => {
     if (error.isTtyError) {
